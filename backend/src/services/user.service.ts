afaeb9556db5f9b8d15613a42dad1484049b282b
Cristian Gómez <cgomez@queplan.cl>
2021-04-13 01:59:30 -0400

    [Back] CHG create validation to create account when user try create new recipient but this already exist, the functionality validate if the account already exist to else create account

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -7,7 +7,9 @@ export const createUser = async (user: TypeUser): Promise<TypeUser> => {
    const _user = await findUserByEmail(user?.email) || await findUserByRut(user?.rut)
    const userRecord = _user ? _user : { isNewRecord: true, ...((await User.create(user)) as any)?.dataValues }
 
-   _createBankAccount(user, userRecord.id)
+   if (!_user || (_user && !_user.bankAccountList?.find((item: any) => item.accountNumber == (user as any).accountNumber)))
+      _createBankAccount(user, userRecord.id)
+
    return userRecord
 }
 
@@ -17,23 +19,23 @@ const _createBankAccount = (user: any, userId: string) => {
       createBankAccount({ accountNumber, bank, user_id: userId, bank_account_type_id: bankAccountType.id })
 }
 
-export const retrieveUserList = async (): Promise<User[]> => User.findAll({
-   include: [{
-      model: BankAccount,
-      as: 'bankAccountList',
-      attributes: ['id', 'accountNumber'],
-      include: [{
-         model: BankAccountType,
-         as: 'bankAccountType',
-         attributes: ['id', 'name']
-      }]
-   }]
-})
+export const retrieveUserList = async (): Promise<User[]> => User.findAll({ include: includeUserQuery })
 
 export const findUserByEmail = async (email: string): Promise<User | any> => {
-   return User.findOne({ where: { email } })
+   return User.findOne({ where: { email }, include: includeUserQuery })
 }
 
 export const findUserByRut = async (rut: number): Promise<User | any> => {
-   return User.findOne({ where: { rut } })
+   return User.findOne({ where: { rut }, include: includeUserQuery })
 }
+
+const includeUserQuery = [{
+   model: BankAccount,
+   as: 'bankAccountList',
+   attributes: ['id', 'accountNumber'],
+   include: [{
+      model: BankAccountType,
+      as: 'bankAccountType',
+      attributes: ['id', 'name']
+   }]
+}]
